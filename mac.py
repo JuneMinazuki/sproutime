0087297df6a64006d272ad860db516c807fb93de
JuneMinazuki <xtfrancis@gmail.com>
2025-04-11 20:39:55 +0800

    Update code on app detection, pending to fix line not updating correctly

--- mac.py
+++ mac.py
@@ -1,32 +1,41 @@
-import tkinter as tk
+import customtkinter as ctk
 import AppKit # type: ignore
 
+#DEBUG
+DEBUG = 1
+
+app_name_list = []
+app_time_list = []
+sleep_time = 1 if DEBUG else 60
+app_index = 0
+
 #App Info
-window = tk.Tk()
+window = ctk.CTk()
 window.geometry("1080x720")
 window.title("Sproutime")
 
-def get_app_open_durations():
-    running_apps = AppKit.NSWorkspace.sharedWorkspace().runningApplications()
-    app_durations = {}
+def get_active_app_name():    
+    app = AppKit.NSWorkspace.sharedWorkspace().activeApplication()
+    return app['NSApplicationName']
 
-    for app in running_apps:
-        if not app.isHidden(): # Check if the application is hidden.
-            app_name = app.localizedName()
-            launch_date = app.launchDate()
+def update_list():
+    app_name = get_active_app_name()
+    if app_name in app_name_list:
+        app_index = app_name_list.index(app_name)
+        app_time_list[app_index] += sleep_time
 
-            if app_name and launch_date:
-                now = AppKit.NSDate.date()
-                duration = now.timeIntervalSinceDate_(launch_date)
-                duration = round(duration / 3600, 2)
-                app_durations[app_name] = duration
+        app_list_TB.delete(f"{app_index}.0", f"{app_index}.end")   #error here 
+        app_list_TB.insert(f"{app_index}.0", f'{app_name_list[app_index]}: {app_time_list[app_index]} seconds')
+    else:
+        app_name_list.append(app_name)
+        app_time_list.append(sleep_time)
+        app_list_TB.insert(f"{len(app_name_list)+1}.0", f'{app_name_list[-1]}: {app_time_list[-1]} seconds')
 
-    return app_durations
+    window.after(sleep_time*1000, update_list)
 
-listbox = tk.Listbox(window, width=1080, height=720)
-listbox.pack()
+app_list_TB = ctk.CTkTextbox(window, width=1080, height=720)
+app_list_TB.grid(row=0, column=0)
 
-for app_name, duration in get_app_open_durations().items():
-    listbox.insert(tk.END, f'{app_name}: {duration} hours')
+update_list()
 
 window.mainloop()
