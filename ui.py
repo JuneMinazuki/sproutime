3f203105331f0e530e53ffee161b06661123bdc0
ChinZhengHong <hong99963@gmail.com>
2025-04-21 10:18:15 +0800

    Using customtkinter

--- ui.py
+++ ui.py
@@ -1,2 +1,13 @@
-import customtkinter as ctk
-import sys
+import customtkinter
+
+def button_callback():
+    print("Button clicked!")
+
+app = customtkinter.CTk()
+app.geometry("1080x720")
+app.title("Sproutime")
+
+button = customtkinter.CTkButton(app, text="Try to click me", command=button_callback)
+button.pack(padx=20, pady=20)
+
+app.mainloop()
