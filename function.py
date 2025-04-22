59b0534106830da82b613fc7f226ac15e676eb89
ChinZhengHong <hong99963@gmail.com>
2025-04-22 17:56:26 +0800

    determine the OS

--- function.py
+++ function.py
@@ -1,1 +1,8 @@
 from window import window
+from mac import mac
+
+def win():
+    window()
+    
+def mac():
+    mac()
--- ui.py
+++ ui.py
@@ -1,15 +1,37 @@
 import customtkinter
 import sys
-from window import window
+from function import *
+import os
+
+x=0
 
 def button_callback():
     print("Start")
-    window()
-
-
+    if x==1:
+        win()
+    elif x==2:
+        mac()
+    else:
+        print("Sorry, this OS is not supported yet")
+    
+# Make application window
 app = customtkinter.CTk()
 app.geometry("1080x720")
 app.title("Sproutime --main menu")
+
+# Determine the operating system
+os_name = os.name  # Returns 'nt' for Windows, 'posix' for Mac
+if os_name == "nt":
+    print("Running on Windows")
+    x=1
+
+elif os_name == "posix":
+    print("Running on macOS")
+    x=2
+
+else:
+    print(f"Unknown OS: {os_name}")
+    x=3
 
 button = customtkinter.CTkButton(app, text="Start", command=button_callback)
 button.pack(padx=20, pady=20)
