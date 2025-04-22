815456763c0755dba515a7946ee61062c9b54e2e
ChinZhengHong <hong99963@gmail.com>
2025-04-22 17:36:24 +0800

    add function

--- ui.py
+++ ui.py
@@ -1,13 +1,17 @@
 import customtkinter
+import sys
+from window import window
 
 def button_callback():
-    print("Button clicked!")
+    print("Start")
+    window()
+
 
 app = customtkinter.CTk()
 app.geometry("1080x720")
-app.title("Sproutime")
+app.title("Sproutime --main menu")
 
-button = customtkinter.CTkButton(app, text="Try to click me", command=button_callback)
+button = customtkinter.CTkButton(app, text="Start", command=button_callback)
 button.pack(padx=20, pady=20)
 
 app.mainloop()
