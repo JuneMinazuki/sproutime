129a117c0085b88a9dfb2eb274af9d565a6225b8
ChinZhengHong <hong99963@gmail.com>
2025-04-23 21:05:28 +0800

    a

--- window.py
+++ window.py
@@ -1,7 +1,7 @@
-def window():   
+def window():    
     import customtkinter as ctk
     import sys
-
+    
     #Check OS
     if sys.platform == 'darwin':
         import AppKit
@@ -9,25 +9,20 @@ def window():
         import psutil
         import win32gui
         import win32process
-        from pywinauto.application import Application
-
+    
     #DEBUG
     DEBUG = 1 #Use this to lower the time check for app from minute to second to save time
     
-    global temp_quest_app, temp_quest_time
-
     app_dict = {}
+    app_time_list = []
     sleep_time = 1 if DEBUG else 60
-    temp_quest_app = ""
-    temp_quest_time = ""
-
+    app_index = 1
+    
     #App Info
     window = ctk.CTk()
     window.geometry("1080x720")
     window.title("Sproutime")
-    window.columnconfigure(1, weight=1)
-    window.columnconfigure(1, weight=1)
-
+    
     def get_active_app_name():
         if sys.platform == 'darwin':
             appName = AppKit.NSWorkspace.sharedWorkspace().activeApplication()['NSApplicationName']
@@ -35,13 +30,10 @@ def window():
         elif sys.platform == 'win32': #check if correct for win
             foregroundApp = win32gui.GetForegroundWindow()
             appTitle = win32gui.GetWindowText(foregroundApp)
-            appName = appTitle.split(" - ")[-1]
-            if appName == "Google Chrome": 
-                tabName = get_active_tab_name()
-                appName = " - ".join([appName, tabName])
+            appName = appTitle.split("-")[-1].strip()
             
         return appName
-
+    
     def get_all_app_list():
             app_list = []
             
@@ -57,115 +49,46 @@ def window():
                 PID_list = []
                 for process in psutil.process_iter(['pid', 'name']): # Loops through all running processes 
                     pid = process.info['pid']
-
+    
                     def enumWindowsArguments(handle, __): # This will be called for each top-level window to exclude all other background processes (refer to EnumWindows)
                         threadID, foundPID = win32process.GetWindowThreadProcessId(handle) # Get the Process ID for the current window handle
-
+    
                         if foundPID == pid and win32gui.IsWindowVisible(handle): # This checks if it is actually a visible window
                             runningAppName = process.info['name'].split(".")[0].capitalize()
                             if runningAppName not in app_list:
                                 app_list.append(runningAppName)
                                 PID_list.append(pid) # I think this will be needed later when we implement the user task setup
-
-                    win32gui.EnumWindows(enumWindowsArguments, None) # Enumerate all top-level windows
-
-            return app_list
-
-    def get_active_tab_name():
-        if sys.platform == "darwin":
-            pass
-
-        elif sys.platform == "win32":
-            try:
-                foregroundApp = win32gui.GetForegroundWindow()
-                TID, PID = win32process.GetWindowThreadProcessId(foregroundApp)
-                chromeApp = Application(backend = "uia").connect(process = PID)
-                topWindow = chromeApp.top_window()
-                url = topWindow.child_window(title = "Address and search bar", control_type = "Edit").get_value() # URL is here
-
-                tabName = url.split("/")[0].split(".")[-2].capitalize()
-            except Exception:
-                tabName = "URL not detected"
-
-        return tabName
     
-    def combobox_callback(choice):
-        global temp_quest_app
-        temp_quest_app = choice
-            
-    def timebox_callback(choice):
-        global temp_quest_time
-        temp_quest_time = choice
-            
-    def save_quest_time():
-        global temp_quest_app, temp_quest_time
-        try:
-            with open("quest_log.txt", 'r') as log:
-                lines = log.readlines()
-        except FileNotFoundError:
-            with open("quest_log.txt", "a") as log:
-                log.write(f"{temp_quest_app} : {temp_quest_time}\n")
-            update_quest_list()
-            return
-
-        updated_lines = []
-        found = False
-        for line in lines:
-            if line.startswith(temp_quest_app):
-                parts = line.split(':', 1)
-                if len(parts) == 2:
-                    updated_line = f"{temp_quest_app} : {temp_quest_time}\n"
-                    updated_lines.append(updated_line)
-                    found = True
-                else:
-                    updated_lines.append(line)
-            else:
-                updated_lines.append(line)
-
-        if found:
-            with open("quest_log.txt", 'w') as log:
-                log.writelines(updated_lines)
-        else:
-            with open("quest_log.txt", "a") as log:
-                log.write(f"{temp_quest_app} : {temp_quest_time}\n")
-        update_quest_list()
-
-    def update_quest_list():
-        try:
-            quest_list_TB.delete("0.0", "end")
-            with open("quest_log.txt", "r") as log:
-                for line in log:
-                    quest_list_TB.insert("0.0", f'{line.strip()}\n')
-        except FileNotFoundError:
-            pass
-
+                    win32gui.EnumWindows(enumWindowsArguments, None) # Enumerate all top-level windows
+    
+            return app_list
+    
     def update_loop(): #this is the while true loop
         app_name = get_active_app_name()
         if app_name in app_dict:
             app_index = list(app_dict.keys()).index(app_name) +1
             app_dict[app_name] += sleep_time
-
+    
             app_list_TB.delete(f"{app_index}.0", f"{app_index}.end")
             app_list_TB.insert(f"{app_index}.0", f'{list(app_dict.keys())[app_index -1]}: {app_dict[app_name]} seconds')
         else:
             app_dict[app_name] = sleep_time
             
             app_list_TB.insert(f"end", f'{list(app_dict.keys())[-1]}: {app_dict[app_name]} seconds\n')
-
+    
         window.after(sleep_time*1000, update_loop)
         
     def on_closing(): #when user close the program
         print("Window is closing!") #temp code
-        window.destroy()
+        sys.exit(0)
     
     app_list_TB = ctk.CTkTextbox(window, width=1080, height=720)
     app_list_TB.grid(row=0, column=0)
     
     update_loop()
-
-    window.protocol("WM_DELETE_WINDOW", on_closing) #check for if user close the program
-
-    window.mainloop()
     
-if __name__=="__main__":
-    window()
+    print(get_all_app_list())
+    
+    window.protocol("WM_DELETE_WINDOW", on_closing) #check for if user close the program
+    
+    window.mainloop()
