b708e99749de6642c76e646b415cb6b44f5e9805
JuneMinazuki <xtfrancis@gmail.com>
2025-05-29 12:21:56 +0800

    Fixed point not assign a value

--- sproutime.py
+++ sproutime.py
@@ -378,56 +378,55 @@ class Tabview(ctk.CTkTabview):
                     if conn:
                         conn.close()
 
-        # update label with point
-        self.treeview_label.configure(text=f"Point: {point}")
+                # update label with point
+                self.treeview_label.configure(text=f"Point: {point}")
 
-        # Check if the current theme is dark or light
-        # Get the current appearance mode as "dark" or "light"
-        current_theme = ctk.get_appearance_mode().lower()
-        
+                # Check if the current theme is dark or light
+                # Get the current appearance mode as "dark" or "light"
+                current_theme = ctk.get_appearance_mode().lower()
+                
+                # update image with your own image path if point >=100
+                if point >= 100 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree2_night.jpg")
 
-        # update image with your own image path if point >=100
-        if point >= 100 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree2_night.jpg")
+                elif point >= 100 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree2_day.jpg")
 
-        elif point >= 100 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree2_day.jpg")
+                elif point >= 200 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree3_night.jpg")
+                    
+                elif point >= 200 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree3_day.jpg")
 
-        elif point >= 200 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree3_night.jpg")
-            
-        elif point >= 200 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree3_day.jpg")
+                elif point >= 300 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree4_night.jpg")
+                    
+                elif point >= 300 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree4_day.jpg")
 
-        elif point >= 300 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree4_night.jpg")
-            
-        elif point >= 300 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree4_day.jpg")
+                elif point >= 400 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree5_night.jpg")
 
-        elif point >= 400 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree5_night.jpg")
+                elif point >= 400 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree5_day.jpg")
 
-        elif point >= 400 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree5_day.jpg")
+                elif point >= 500 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree6_night.jpg")
 
-        elif point >= 500 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree6_night.jpg")
+                elif point >= 500 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree6_day.jpg")
 
-        elif point >= 500 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree6_day.jpg")
+                elif point >= 600 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree7_night.jpg")
+                    
+                elif point >= 600 and current_theme == "light":
+                    self.display_image(self.treeview_tab, "img/tree7_day.jpg")
 
-        elif point >= 600 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree7_night.jpg")
-            
-        elif point >= 600 and current_theme == "light":
-            self.display_image(self.treeview_tab, "img/tree7_day.jpg")
+                elif point < 100 and current_theme == "dark":
+                    self.display_image(self.treeview_tab, "img/tree1_night.jpg")
 
-        elif point < 100 and current_theme == "dark":
-            self.display_image(self.treeview_tab, "img/tree1_night.jpg")
-
-        else:
-            self.display_image(self.treeview_tab, "img/tree1_day.jpg")     
+                else:
+                    self.display_image(self.treeview_tab, "img/tree1_day.jpg")     
 
     def update_progress(self):
         global running, app_time_update, sort_type
